53274b80792b139bb7714fa1864ad33d6b17de78
rasujon3@gmail.com <rasujon3@gmail.com>
2024-05-11 01:00:03 +0600

    style add

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,17 +1,46 @@
 <template>
-    <div>Hello from page home</div>
+    <!-- style start -->
+    <div v-for="thread in threads" 
+        :key="thread.id" 
+        class="col-large push-top">
+        <h1>{{ thread.title }}</h1>
+        <div class="post-list">
+            <div class="post" v-for="postId in thread.posts" :key="postId">
+                <div class="user-info">
+                    <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
+                    <a href="#">
+                        <img class="avatar-large" :src="userById(postById(postId).userId).avatar" :alt="userById(postById(postId).userId).name">
+                    </a>
+                    <p class="desktop-only text-small">107 posts</p>
+                </div>
+                <div class="post-content">
+                    <div>
+                        <p>
+                            {{ postById(postId).text }}
+                        </p>
+                    </div>
+                </div>
+                <div class="post-date text-faded">
+                    {{ postById(postId).publishedAt }}
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- style end -->
+    <!-- <div>Hello from page home</div>
     <div v-for="thread in threads" :key="thread.id">
         <h2>{{ thread.title }}</h2>
         <div v-for="postId in thread.posts" :key="postId">
         <p>{{ userById(postById(postId).userId).name }}</p>
         <p>{{ postById(postId).text }}</p>
     </div>
-    </div>
+    </div> -->
 </template>
 
-<!-- <script>
+<script>
 import sourceData from '@/data.json';
-console.log('sourceData',sourceData.users);
+// console.log('sourceData',sourceData.users);
 export default {
     data () {
     return {
@@ -29,8 +58,8 @@ export default {
     }
   }
 }
-</script> -->
-<script setup>
+</script>
+<!-- <script setup>
 import sourceData from '@/data.json';
 import { ref} from 'vue';
 
@@ -44,7 +73,7 @@ function postById (postId) {
 function userById (userId) {
         return users.value.find(u => u.id === userId)
     }
-</script>
+</script> -->
 
 <style scoped>
 
